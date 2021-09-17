1a1f02297e510d7409ebd5c451fb54a3838ae926
Bruno Razera <bcr@vortx.com.br>
2021-09-17 16:56:54 -0300

    feat: adiciona serviço de armazenamento de segredos da AWS

--- src/config/secrets.js
+++ src/config/secrets.js
@@ -5,8 +5,11 @@ module.exports.SecretsManager = {
 
   async getSecretValue(secretName) {
     try {
-      const { SecretString } = await this.client.getSecretValue({ SecretId: secretName }).promise()
-      return JSON.parse(SecretString)
+      const { SecretString, SecretBinary } = await this.client.getSecretValue({ SecretId: secretName }).promise()
+
+      if (SecretString) return SecretString
+
+      return Buffer(SecretBinary, 'base64').toString('ascii');
     } catch (error) {
       console.log(error)
     }
@@ -14,12 +17,12 @@ module.exports.SecretsManager = {
 
   async getDBSecrets() {
     return {
-      DB_PORT: this.getSecretValue('stg/vxapi-aws-kms-DB_PORT'),
-      DB_CLIENT: this.getSecretValue('stg/vxapi-aws-kms-DB_CLIENT'),
-      DB_DATABASE: this.getSecretValue('stg/vxapi-aws-kms-DB_DATABASE'),
-      DB_USER: this.getSecretValue('stg/vxapi-aws-kms-DB_USER'),
-      DB_HOST: this.getSecretValue('stg/vxapi-aws-kms-DB_HOST'),
-      DB_PASSWORD: this.getSecretValue('stg/vxapi-aws-kms-DB_PASSWORD')
+      DB_PORT: await this.getSecretValue('stg/vxapi-aws-kms-DB_PORT'),
+      DB_CLIENT: await this.getSecretValue('stg/vxapi-aws-kms-DB_CLIENT'),
+      DB_DATABASE: await this.getSecretValue('stg/vxapi-aws-kms-DB_DATABASE'),
+      DB_USER: await this.getSecretValue('stg/vxapi-aws-kms-DB_USER'),
+      DB_HOST: await this.getSecretValue('stg/vxapi-aws-kms-DB_HOST'),
+      DB_PASSWORD: await this.getSecretValue('stg/vxapi-aws-kms-DB_PASSWORD')
     }
   }
 }
