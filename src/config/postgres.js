c6d27326e3ac6dd28f64082386f76ba23a43d0b3
Bruno Razera <bcr@vortx.com.br>
2021-09-17 16:56:22 -0300

    feat: importa parâmetros de conexão do banco provenientes do serviço de segredos da AWS

--- src/config/postgres.js
+++ src/config/postgres.js
@@ -1,12 +1,15 @@
 const secretsManager = require('./secrets').SecretsManager
-const dbSecrets = secretsManager.getDBSecrets()
 
-module.exports = {
-  client: dbSecrets.DB_CLIENT || 'pg',
-  connection: {
-    host: dbSecrets.DB_HOST || '127.0.0.1',
-    user: dbSecrets.DB_USER || 'postgres',
-    password: dbSecrets.DB_PASSWORD || 'postgres',
-    database: dbSecrets.DB_DATABASE || 'todolist_on_herbs_db'
+module.exports = async () => {
+  const dbSecrets = await secretsManager.getDBSecrets()
+
+  return {
+    client: dbSecrets.DB_CLIENT || 'pg',
+    connection: {
+      host: dbSecrets.DB_HOST || '127.0.0.1',
+      user: dbSecrets.DB_USER || 'postgres',
+      password: dbSecrets.DB_PASSWORD || 'postgres',
+      database: dbSecrets.DB_DATABASE || 'todolist_on_herbs_db'
+    }
   }
 }
