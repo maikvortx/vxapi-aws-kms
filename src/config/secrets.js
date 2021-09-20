83f93f67a27acf3a77541d47597ee334d791e41f
Bruno Razera <bcr@vortx.com.br>
2021-09-20 17:25:17 -0300

    refactor: melhora aquisição de segredos do banco de dados

--- src/config/secrets.js
+++ src/config/secrets.js
@@ -6,23 +6,14 @@ module.exports.SecretsManager = {
   async getSecretValue(secretName) {
     try {
       const { SecretString, SecretBinary } = await this.client.getSecretValue({ SecretId: secretName }).promise()
-
-      if (SecretString) return SecretString
-
-      return Buffer(SecretBinary, 'base64').toString('ascii');
+      const secretString = SecretString ? SecretString : Buffer(SecretBinary, 'base64').toString('ascii')
+      return JSON.parse(secretString)
     } catch (error) {
       console.log(error)
     }
   },
 
   async getDBSecrets() {
-    return {
-      DB_PORT: await this.getSecretValue('stg/vxapi-aws-kms-DB_PORT'),
-      DB_CLIENT: await this.getSecretValue('stg/vxapi-aws-kms-DB_CLIENT'),
-      DB_DATABASE: await this.getSecretValue('stg/vxapi-aws-kms-DB_DATABASE'),
-      DB_USER: await this.getSecretValue('stg/vxapi-aws-kms-DB_USER'),
-      DB_HOST: await this.getSecretValue('stg/vxapi-aws-kms-DB_HOST'),
-      DB_PASSWORD: await this.getSecretValue('stg/vxapi-aws-kms-DB_PASSWORD')
-    }
+    return this.getSecretValue('stg/vxapi-aws-kms/db')
   }
 }
